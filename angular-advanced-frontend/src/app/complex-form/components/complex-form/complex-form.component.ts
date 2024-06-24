0e9058ed323a9859c5fca5b7661537d783d542e1
TBERT31 <berteau.thomas@hotmail.fr>
2024-06-24 19:56:23 +0200

    Put value changes due to contact user preferences

--- angular-advanced-frontend/src/app/complex-form/components/complex-form/complex-form.component.ts
+++ angular-advanced-frontend/src/app/complex-form/components/complex-form/complex-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable, map, startWith } from 'rxjs';
 
 @Component({
   selector: 'app-complex-form',
@@ -9,23 +10,74 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 export class ComplexFormComponent implements OnInit {
 
   mainForm!: FormGroup;
+  personalInfoForm!: FormGroup;
+  contactPreferenceCtrl!: FormControl;
+  emailCtrl!: FormControl;
+  confirmEmailCtrl!: FormControl;
+  emailForm!: FormGroup;
+  phoneCtrl!: FormControl;
+  passwordCtrl!: FormControl;
+  confirmPasswordCtrl!: FormControl;
+  loginInfoForm!: FormGroup;
+
+  showEmailCtrl$!: Observable<boolean>;
+  showPhoneCtrl$!: Observable<boolean>;
 
   constructor(
     private formBuilder: FormBuilder,
   ) { }
 
   ngOnInit(): void {
+    this.initFormControls();
     this.initMainForm();
+    this.initFormObservables();
   }
 
-  initMainForm(): void {  
+  private initMainForm(): void {  
     this.mainForm = this.formBuilder.group({
-
+      personalInfo: this.personalInfoForm,
+      contactPreference: this.contactPreferenceCtrl,
+      email: this.emailForm,
+      phone: this.phoneCtrl,
+      loginInfo: this.loginInfoForm, 
     });
   }
 
-  onSubmitForm(): void {
-    console.log(this.mainForm.value);
+  private initFormControls(): void {
+    this.personalInfoForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+    });
+    this.contactPreferenceCtrl = this.formBuilder.control('email');
+    this.emailCtrl = this.formBuilder.control('');
+    this.confirmEmailCtrl = this.formBuilder.control('');
+    this.emailForm = this.formBuilder.group({
+      email: this.emailCtrl,
+      confirmEmail: this.confirmEmailCtrl,
+    });
+    this.phoneCtrl = this.formBuilder.control('');
+    this.passwordCtrl = this.formBuilder.control('', Validators.required);
+    this.confirmPasswordCtrl = this.formBuilder.control('', Validators.required);
+    this.loginInfoForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: this.passwordCtrl,
+      confirmPassword: this.confirmPasswordCtrl,
+    });
+  }
+
+  private initFormObservables(): void {
+    this.showEmailCtrl$ = this.contactPreferenceCtrl.valueChanges.pipe(
+      startWith(this.contactPreferenceCtrl.value),
+      map(preference => preference === 'email'),
+    );
+    this.showPhoneCtrl$ = this.contactPreferenceCtrl.valueChanges.pipe(
+      startWith(this.contactPreferenceCtrl.value),
+      map(preference => preference === 'phone')
+    );
+  }
+
+  onSubmitForm() {
+    
   }
 
 }
